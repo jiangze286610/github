21561503b8f477266a8f191f2a707189f3ce2c2e
jiangze286610 <2337537837@qq.com>
2024-03-21 18:40:51 +0800

    tb_seg_595_static

--- sim/test_tb.v
+++ sim/test_tb.v
@@ -1,5 +1,7 @@
 `timescale  1ns/1ns
+
 module  tb_seg_595_static();
+
   //wire  define
   wire    stcp    ;   //输出数据存储寄时钟
   wire    shcp    ;   //移位寄存器的时钟输入
@@ -23,17 +25,16 @@ module  tb_seg_595_static();
   always  #10 sys_clk <=  ~sys_clk;
 
   //重新定义参数值，缩短仿真时间
-  defparam    seg_595_static_inst.seg_static_inst.cnt_1s_max    =   10;
+  defparam    seg_top.seg_static.cnt_1s_max    =   10;
 
-  seg_top seg_595_static_inst
+  seg_top seg_top
           (
             .sys_clk     (sys_clk   ),  //系统时钟，频率50MHz
             .sys_rst_n   (sys_rst_n ),  //复位信号，低电平有效
-
             .stcp        (stcp      ),   //输出数据存储寄时钟
             .shcp        (shcp      ),   //移位寄存器的时钟输入
             .ds          (ds        ),   //串行数据输入
-            .oe          (oe        )    //输出使能信号
+            .oe          ( oe   )    //输出使能信号
           );
 
 endmodule
